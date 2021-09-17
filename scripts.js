51e86ff5cf2e31cf4471f9b5f5b8c98952a1efb6
JLZ-coder <chengliu@ucm.es>
2021-09-17 02:13:45 +0200

    calculator finished

--- scripts.js
+++ scripts.js
@@ -36,40 +36,116 @@ function operate(operator, x, y) {
 }
 
 //Document elements
-let alreadyPressedDot = false;
-
 const display = document.querySelector("#display");
 const lastResultDisplay = document.querySelector("#lastResult");
+
+let operands = [];
+let lastOperator = null;
+let operatorButtonActive = false;
+
+const clearButton = document.querySelector(".clearButton");
+clearButton.addEventListener("click", function() {
+    if (this.textContent === "C") {
+        clearElement(display, "0");
+        this.textContent = "AC";
+    }
+    else {
+        clearElement(display, "0");
+        operands = [];
+        lastOperator = null;
+        deactivateAllOperatorButtons();
+        operatorButtonActive = false;
+    }
+});
 
 const buttonList = document.querySelectorAll(".digitButton");
 buttonList.forEach(button => {
     if (button.getAttribute("id") === "dotButton") {
         button.addEventListener("click", () => {
-            if (!alreadyPressedDot && display.textContent != "") {
-                addToElement(display, button.textContent);
-                alreadyPressedDot = true;
+            if (!display.textContent.includes(".")) {
+                display.textContent += button.textContent;
+            }
+            
+            clearButton.textContent = "C";
+        });
+    }
+    else {
+        button.addEventListener("click", () => {
+            if (operatorButtonActive || display.textContent === "0") {
+                display.textContent = "";
+                operatorButtonActive = false;
+            }
+            display.textContent += button.textContent;
+            //lastOperator = getActiveOperator();
+            deactivateAllOperatorButtons();
+            if (display.textContent != "0") {
+                clearButton.textContent = "C";
+            }
+        });
+    }
+});
+
+const operatorButtonList = document.querySelectorAll(".operatorButton");
+operatorButtonList.forEach(button => {
+    if (button.textContent === "=") {
+        button.addEventListener("click", () => {
+            if (lastOperator != null) {
+                operatorButtonActive = true;
+                operands.push(+display.textContent);
+                deactivateAllOperatorButtons();
+                if (operands.length == 2) {
+                    const result = operate(lastOperator, operands[0], operands[1]);
+                    if (result == Infinity || result == isNaN) {
+                        operands = [];
+                        display.textContent = "Error";
+                    }
+                    else {
+                        operands = [result];
+                        display.textContent = result;
+                    }
+                }
             }
         });
     }
     else {
         button.addEventListener("click", () => {
-            addToElement(display, button.textContent);
+            deactivateAllOperatorButtons();
+            button.classList.add("active");
+            if (!operatorButtonActive) {
+                operatorButtonActive = true;
+                operands.push(+display.textContent);
+                if (operands.length === 2) {
+                    const result = operate(lastOperator, operands[0], operands[1]);
+                    if (result == Infinity || result == isNaN) {
+                        operands = [];
+                        display.textContent = "Error";
+                    }
+                    else {
+                        operands = [result];
+                        display.textContent = result;
+                    }
+                }
+            }
+            lastOperator = button.textContent;
         });
     }
 });
 
-const clearButton = document.querySelector(".clearButton");
-clearButton.addEventListener("click", () => {
-    alreadyPressedDot = false;
-    clearElement(display);
-});
-
-const operatorButtonList = document.querySelectorAll(".operatorButton");
-operatorButtonList.forEach(button => {
-    button.addEventListener("click", () => {
-        operateAndAddToElement(button.textContent);
+function deactivateAllOperatorButtons() {
+    operatorButtonList.forEach(button => {
+        button.classList.remove("active");
     });
-});
+}
+
+function getActiveOperator() {
+    let operator = null;
+    operatorButtonList.forEach(button => {
+        if (button.classList.contains("active")) {
+            operator = button.textContent;
+        }
+    });
+    return operator;
+}
 
 function addToElement(element, digit) {
     element.textContent += digit;
@@ -81,32 +157,36 @@ function clearElement(element, digit="") {
 
 function operateAndAddToElement(operator) {
     if (display.textContent != "") {
-        // if (lastResultDisplay.textContent === "" || lastResultDisplay.textContent.endsWith("=")) {
-        //     clearElement(lastResultDisplay);
-        //     addToElement(lastResultDisplay, `${display.textContent} ${operator}`);
-        //     clearElement(display);
-        // }
-        // else {
-        //     let lastOp = lastResultDisplay.textContent.split(" ");
-        //     let result = operate(lastOp[1], +lastOp[0], +display.textContent);
-        //     lastResultDisplay.textContent = `${lastOp[0]} ${lastOp[1]} ${display.textContent} = ${result} ${operator}`;
-        //     clearElement(display);
-        //     // if (operator === "=") {
-        //     //     lastResultDisplay.textContent = `${lastOp[0]} ${lastOp[1]} ${display.textContent} ${operator} ${result}`;
-        //     //     clearElement(display);
-        //     //     addToElement(display, result);
-        //     // }
-        //     // else {
-        //     //     lastResultDisplay.textContent = result + " " + operator;
-        //     //     clearElement(display);
-        //     // }
-        // }
+        if (lastResultDisplay.textContent === "") {
+            addToElement(lastResultDisplay, `${display.textContent} ${operator}`);
+            clearElement(display);
+        }
+        else {
+            let lastOp = lastResultDisplay.textContent.split(" ");
+            let lastOpLastIndex = lastOp.length - 1;
 
-        let lastOp = lastResultDisplay.textContent.split(" ");
-        let result = operate(lastOp[1], +lastOp[0], +display.textContent);
-        clearElement(lastResultDisplay);
-        lastResultDisplay.textContent = `${lastOp[0]} ${lastOp[1]} ${display.textContent} = ${result} ${operator}`;
-        clearElement(display);
+            if (lastResultDisplay.textContent.endsWith("= ")) {
+                clearElement(lastResultDisplay);
+                addToElement(lastResultDisplay, `${display.textContent} ${operator}`);
+                clearElement(display);
+            }
+            else {
+                let result = operate(lastOp[lastOpLastIndex], +lastOp[lastOpLastIndex - 1], +display.textContent);
+                
+                clearElement(lastResultDisplay);
+
+                lastResultDisplay.textContent = `${lastOp[lastOpLastIndex - 1]} ${lastOp[lastOpLastIndex]} ${display.textContent} = `;
+
+                clearElement(display);
+
+                if (operator === "=") {
+                    addToElement(display, result);
+                }
+                else {
+                    addToElement(lastResultDisplay, ` ${result} ${operator}`);
+                }
+            }
+        }
     }
 }
 
